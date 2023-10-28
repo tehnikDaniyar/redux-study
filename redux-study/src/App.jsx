c74fb35b43225eb269011b9f54362a472b42a2ee
Danik <tehnikdaniyar@gmail.com>
2023-10-28 23:04:55 +0600

    create store and reducer

--- redux-study/src/App.jsx
+++ redux-study/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const cash = useSelector((state) => state.cash);
+  const dispatch = useDispatch();
+
+  function addCash(cash) {
+    dispatch({ type: 'ADD CASH', payload: cash });
+  }
+
+  function getCash(cash) {
+    dispatch({ type: 'GET CASH', payload: cash });
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='wrapper'>
+        <div className='output'>{cash}</div>
+        <button onClick={() => addCash(Number(prompt()))}>ADD CASH</button>
+        <button onClick={() => getCash(Number(prompt()))}>GET CASH</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- redux-study/src/main.jsx
+++ redux-study/src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const defaultState = {
+  cash: 0,
+};
+
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'ADD CASH':
+      return { ...state, cash: state.cash + action.payload };
+    case 'GET CASH':
+      return { ...state, cash: state.cash - action.payload };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>,
+);
